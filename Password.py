09b9d3ad0be151a680c86533936d92192b5849dc
Amusementman man <harleytoshikian@gmail.com>
2025-02-06 19:52:11 +0000

    message

---
+++ Password.py
@@ -0,0 +1,26 @@
+import random
+import string
+import requests
+
+#function that generates a single random character
+def random_character():
+    choices = string.ascii_letters + string.digits + string.punctuation
+    return random.choice(choices)
+    
+
+passwordLength = int(input("What length would you like your password to be?"))
+
+def generate_strong_password ():
+    password = ""
+    for i in range(passwordLength):
+        password = password + random_character
+    print(password)
+
+generate_strong_password()
+
+def fetch_word():
+    response = requests.get("https://random-word-api.herokuapp.com/word?length=6")
+    word = response.json()[0]
+    return word
+
+print(fetch_word())
